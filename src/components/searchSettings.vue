a93d52e34c8fed3bc9001ff5b85d2c4a7b9f1283
Rómeó Mihálovics <romeo@mihalovics.com>
2020-06-14 21:53:28 +0200

    "forget to stage" files lol

---
+++ src/components/searchSettings.vue
@@ -0,0 +1,265 @@
+<template>
+  <div class="search-settings">
+    <div class="row mx-2 py-2">
+      <div class="col-md-3 text-left d-flex align-items-center">Source</div>
+      <div class="col-md-6 text-right ml-auto d-flex align-items-center">
+        <div class="dropdown d-flex align-items-center" @click="dropdownSource">
+          <div class="current">
+            <span ref="source" class="position-static">Marketplaces (eBay, Amazon)</span>
+            <span class="ti-angle-down"></span>
+          </div>
+          <transition name="dropdown-transition">
+            <ul class="list-unstyled" v-if="showSource" @click="setSource($event.target)">
+              <li>
+                Please select a source
+              </li>
+              <li>
+                Marketplaces (eBay, Amazon)
+              </li>
+              <li>
+                Something else
+              </li>
+            </ul>
+          </transition>
+        </div>
+      </div>
+    </div>
+    <div class="row row-cols-md-2 mx-2 py-2">
+      <div class="col text-left">Completed listings only</div>
+      <div class="col text-right">
+        <label class="switch">
+          <input type="checkbox" checked>
+          <div></div>
+        </label>
+      </div>
+    </div>
+    <div class="row row-cols-md-2 mx-2 py-2">
+      <div class="col text-left">Sold items only</div>
+      <div class="col text-right">
+        <label class="switch">
+          <input type="checkbox">
+          <div></div>
+        </label>
+      </div>
+    </div>
+    <div class="row mx-2 py-2">
+      <div class="col-md-3 text-left d-flex align-items-center">Condition</div>
+      <div class="col-md-6 text-right ml-auto d-flex align-items-center">
+        <div class="dropdown d-flex align-items-center" @click="dropdownCondition">
+          <div class="current">
+            <span ref="condition" class="position-static">Please select a condition</span>
+            <span class="ti-angle-down"></span>
+          </div>
+          <transition name="dropdown-transition">
+            <ul class="list-unstyled" v-if="showCondition" @click="setCondition($event.target)">
+              <li>
+                Please select a condition
+              </li>
+              <li>
+                New
+              </li>
+              <li>
+                Used
+              </li>
+            </ul>
+          </transition>
+        </div>
+      </div>
+    </div>
+    <div class="row mx-2 py-2">
+      <div class="col-md-3 text-left d-flex align-items-center">Buying format</div>
+      <div class="col-md-6 text-right ml-auto">
+        <div class="dropdown d-flex align-items-center" @click="dropdownFormat">
+          <div class="current">
+            <span ref="format" class="position-static">Buy it now</span>
+            <span class="ti-angle-down"></span>
+          </div>
+          <transition name="dropdown-transition">
+            <ul class="list-unstyled" v-if="showFormat" @click="setFormat($event.target)">
+              <li>
+                Please select a buying format
+              </li>
+              <li>
+                Buy it now
+              </li>
+              <li>
+                Pre Order
+              </li>
+            </ul>
+          </transition>
+        </div>
+      </div>
+    </div>
+    <div class="row row-cols-md-2 mx-2 py-2">
+      <div class="col-md-3 text-left">EAN</div>
+      <div class="col-md-6 text-right ml-auto">
+        <input class="settings-input" placeholder="Enter EAN to search for">
+      </div>
+    </div>
+    <div class="row row-cols-md-2 mx-2 py-2">
+      <div class="col-md-3 text-left">ISBN</div>
+      <div class="col-md-6 text-right ml-auto">
+        <input class="settings-input" placeholder="Enter ISBN to search for">
+      </div>
+    </div>
+    <div class="text-center my-3">
+      <button class="searchBtn" @click="showSettings">Search</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../scss/_varibles.scss';
+
+.search-settings {
+  width: calc(100% + 2px);
+  height: 100vh;
+  background-color:$color_text_light;
+  padding: 0;
+  margin: 0;
+  left: -1px;
+  right: 0;
+  top:54px;
+  position: absolute !important;
+  border-left: 1px solid $color_topnav_border;
+  border-top: 1px solid $color_topnav_border;
+  border-bottom: 1px solid $color_topnav_border;
+  & div {
+    min-width: unset !important;
+  }
+  & .row {
+    border-bottom:1px solid $color_topnav_border;
+  }
+  & .dropdown {
+    width: 100%;
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    color:$color_text_dark;
+    & div {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      display: inline-block;
+      width: 100%;
+      background-color: $color_text_light;
+      border-radius: 3px;
+      padding-right: 20px;
+      & span:not(.by) {
+        top:8px;
+        right: 0;
+      }
+    }
+    & ul {
+      z-index: 3;
+      position: absolute;
+      background-color: $color_text_light;
+      border: 1px solid $color_topnav_border;
+      white-space:nowrap;
+      right: 0;
+      top:42px;
+      width: 100%;
+      & li {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        text-align: left !important;
+      }
+    }
+  }
+  & .settings-input {
+    border: none;
+    background:transparent;
+    width: 100%;
+    margin-right: 10px;
+    color:$color_text_dark;
+    outline: none;
+  }
+}
+
+.dropdown-transition-enter-active, .dropdown-transition-leave-active {
+  transition: .3s;
+  opacity: 1;
+  transform: scaleY(1);
+  transform-origin: top center;
+}
+
+.dropdown-transition-enter, .dropdown-transition-leave-to {
+  opacity: 0;
+  transform: scaleY(0);
+  transform-origin: top center;
+}
+
+.searchBtn {
+  border:none;
+  padding:5px 40px;
+  background-color: $color_green;
+  color:$color_text_light;
+  border-radius: 3px;
+}
+
+.switch {
+  cursor: pointer;
+  display: inline-block;
+  font-size: 15px;
+  height: 5px;
+  width: 30px;
+  background: $color_grayish_blue;
+  border-radius: 3px;
+  margin-bottom: 5px;
+  & input {
+    position: absolute;
+    opacity: 0;
+  }
+  & div {
+    height: 15px;
+    width: 15px;
+    border-radius: 50%;
+    background: $color_dark_gray;
+    transition: .3s;
+    position: absolute;
+    bottom:5px;
+  }
+  & input:checked + div {
+    transform: translate3d(100%, 0, 0);
+    background-color: $color_blue;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'searchSettings',
+  data () {
+    return {
+      showSource: false,
+      showCondition: false,
+      showFormat: false
+    }
+  },
+  methods: {
+    dropdownSource () {
+      this.showSource = !this.showSource
+    },
+    dropdownCondition () {
+      this.showCondition = !this.showCondition
+    },
+    dropdownFormat () {
+      this.showFormat = !this.showFormat
+    },
+    setSource (target) {
+      this.$refs.source.innerText = target.innerText
+    },
+    setCondition (target) {
+      this.$refs.condition.innerText = target.innerText
+    },
+    setFormat (target) {
+      this.$refs.format.innerText = target.innerText
+    },
+    showSettings () {
+      this.settings = !this.settings
+      this.$root.$emit('showSettings')
+    }
+  }
+}
+</script>
